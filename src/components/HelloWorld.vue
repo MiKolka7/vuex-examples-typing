dbabfa908f4ae1c698e545f84d3cfa6b3943450f
MiKolka7 <kolka.koval@gmail.com>
2020-02-08 17:46:17 +0200

    fix build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -119,9 +119,6 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class HelloWorld extends Vue {
     @Prop() private msg!: string
-
-    created() {
-    }
 }
 </script>
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,11 @@ import Vuex from 'vuex'
 
 import storeProfile, { ProfileState } from './profile.store'
 import storeCommon, { CommonState } from './common.store'
-import { StoreState } from '@/types'
+import { Store } from '@/types'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store<StoreState>({
+export default new Vuex.Store<Store.State>({
     modules: {
         profile: storeProfile,
         common: storeCommon,
